816f8648b4c72c71fb90759f06cc7e1e822ac2d5
JenniG <jennifergaupel@gmail.com>
2019-08-19 19:38:45 -0400

    - updated main

--- backend/src/entities/exam.py
+++ backend/src/entities/exam.py
@@ -2,9 +2,21 @@
 
 from marshmallow import Schema, fields
 
-# ... other import statements ...
+from sqlalchemy import Column, String
 
-# ... Exam class definition ...
+from .entity import Entity, Base
+
+
+class Exam(Entity, Base):
+    __tablename__ = 'exams'
+
+    title = Column(String)
+    description = Column(String)
+
+    def __init__(self, title, description, created_by):
+        Entity.__init__(self, created_by)
+        self.title = title
+        self.description = description
 
 class ExamSchema(Schema):
     id = fields.Number()
